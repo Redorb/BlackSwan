dbffe63be65d896c8e1e61b62f9d96dd3deda9d1
Kenan Warren <kenan.warren@escribir.io>
2017-01-06 22:50:36 -0500

    Refactor and add username logic

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -7,7 +7,10 @@ def join("room:" <> _private_room_id, _params, _socket) do
     {:error, %{reason: "Unauthorized"}}
   end
 def handle_in("new_msg", %{"body" => body}, socket) do
-    broadcast! socket, "new_msg", %{body: body}
+    broadcast! socket, "new_msg", %{
+      body: body,
+      username: socket.assigns.username
+    }
     {:noreply, socket}
   end
 def handle_out("new_msg", payload, socket) do

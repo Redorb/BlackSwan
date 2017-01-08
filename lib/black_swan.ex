c3e475d5583792e188e80f3ca5ce499d61da7bda
Kenan Warren <kenan.warren@escribir.io>
2017-01-07 22:36:04 -0500

    Add online users with presence

--- lib/black_swan.ex
+++ lib/black_swan.ex
@@ -14,6 +14,7 @@ defmodule BlackSwan do
       supervisor(BlackSwan.Endpoint, []),
       # Start your own worker by calling: BlackSwan.Worker.start_link(arg1, arg2, arg3)
       # worker(BlackSwan.Worker, [arg1, arg2, arg3]),
+      supervisor(BlackSwan.Presence, [])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -1,20 +1,34 @@
 defmodule BlackSwan.RoomChannel do
   use BlackSwan.Web, :channel
-def join("room:lobby", payload, socket) do
+  alias BlackSwan.Presence
+
+  def join("room:lobby", payload, socket) do
+    send(self, :after_join)
     {:ok, socket}
   end
-def join("room:" <> _private_room_id, _params, _socket) do
+
+  def join("room:" <> _private_room_id, _params, _socket) do
     {:error, %{reason: "Unauthorized"}}
   end
-def handle_in("new_msg", %{"body" => body}, socket) do
+
+  def handle_in("new_msg", %{"body" => body}, socket) do
     broadcast! socket, "new_msg", %{
       body: body,
       username: socket.assigns.username
     }
     {:noreply, socket}
   end
-def handle_out("new_msg", payload, socket) do
+
+  def handle_out("new_msg", payload, socket) do
     push socket, "new_msg", payload
+    {:noreply, socket}
+  end
+  
+  def handle_info(:after_join, socket) do
+    {:ok, _} = Presence.track(socket, socket.assigns.username, %{
+      online_at: inspect(System.system_time(:seconds))
+    })
+    push socket, "presence_state", Presence.list(socket)
     {:noreply, socket}
   end
 end
